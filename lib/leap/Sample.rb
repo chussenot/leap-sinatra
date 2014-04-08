71219c693d3e8a614fcbf4e009315f97b9d4f158
Clément Hussenot <chussenot@gmail.com>
2014-04-08 14:57:02 -0400

    Update sample

--- lib/leap/Sample.rb
+++ lib/leap/Sample.rb
@@ -1,11 +1,16 @@
 require_relative 'Leap.bundle'
-   controller = Leap::Controller.new()
-   for i in 0..600
-       frame = controller.frame()
-       print "Frame id: ", frame.id()
-       print ", timestamp: ", frame.timestamp()
-       print ", hands: ", frame.hands().count()
-       print ", fingers: ", frame.fingers().count()
-       print ", tools: ", frame.tools.count(), "\n"
-       sleep(0.0166666)
-   end
+
+controller = Leap::Controller.new()
+for i in 0..600
+   
+  frame = controller.frame()
+  x = frame.hand(0).palmNormal.x
+  y = frame.hand(0).palmNormal.y
+
+  print "Frame id: ", frame.id()
+  print ", timestamp: ", frame.timestamp()
+  print ", hands: ", frame.hands().count()
+  print ", x: ", x
+  print ", y: ", y, "\n"
+  sleep(0.0166666)
+end
